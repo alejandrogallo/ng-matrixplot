ce22ea1bedad37b41cf498dba8012ec04b37bf3a
alejandro gallo <alejandroamsg@hotmail.com>
2015-09-28 20:29:46 +0200

    Added javascript object and angular wrapper

--- src/ng-matrixplot.js
+++ src/ng-matrixplot.js
@@ -1,73 +1,53 @@
 
-var app = angular.module("angular-matrixplot",[]);
+var app = angular.module("ng-matrixplot", []);
 
 
-app.controller("MainController", ["$scope", function($scope){
-	$scope.prepareData = function(){
-		var data = []; 
-		$scope.rows=80; 
-		$scope.cols=80 ;
-		var seed= Math.random()*10000;
-		for (var i = 0; i < $scope.rows; i++) {
-			data.push([]);
-			for (var j = 0; j < $scope.cols; j++) {
-				data[i].push(Math.floor(Math.sin(2*3.14*seed*i*j/($scope.rows*$scope.cols*100))*Math.cos(i/seed)*100));
-			};
-		};
-		return data;
-	};
-	$scope.width=600;
-	$scope.height=600;
-	$scope.color=240;
-	$scope.rows = 80; 
-	$scope.cols = 80;
-	$scope.data = $scope.prepareData();
+app.controller("MainController", ["$scope", function ($scope) {
+ $scope.prepareData = function () { 
+  // $scope.data=[[0]];
+  var data = []; 
+  $scope.rows=80; 
+  $scope.cols=80 ;
+  var seed= Math.random()*10000;
+  for (var i = 0; i < $scope.rows; i++) {
+   data.push([]);
+   for (var j = 0; j < $scope.cols; j++) {
+    data[i].push(Math.floor(Math.sin(2*3.14*seed*i*j/($scope.rows*$scope.cols*100))*Math.cos(i/seed)*100));
+   };
+  };
+  return data;
+ };
+
+
+
+ $scope.settings={"data":$scope.prepareData(), "options":{"width": 600 ,"height": 600 ,"color": 240}};
 
 }]);
 
 app.directive("matrixPlot", ["$window", "$parse", function ($window, $parse) {
 
-	return {
-		restrict: "EA",
-		template: "<canvas> </canvas>",
-		link: function(scope, elem, attrs){
+ return {
+  restrict: "EA",
+  template: "<canvas> </canvas>",
+  link: function(scope, elem, attrs){
+   var canvas = elem.find("canvas")[0];
+   var settings = $parse(attrs.settings)(scope);
 
-			var data = $parse(attrs.data)(scope);
-			var parsedColor = $parse(attrs.color)(scope);
-			var maincolor = parsedColor ? parsedColor : "360";
-			// $window.console.log(data);
+   var plot = matrixplot(canvas, settings);
 
-			cols = data[0].length;
-			rows = data.length;
+   scope.$watch(attrs.settings, function(newv, oldv) {
+    if (newv) {
+     // console.log("Not empty");
+     // console.log(newv);
+     plot.refresh(newv); 
+    };
+    
+   },true);
 
-			var rawCanvas = elem.find("canvas");
-			var canvas = $window.d3.select(rawCanvas[0]);
-
-			var width=$parse(attrs.width)(), height=$parse(attrs.height)();
-			// $window.console.log(width, height);
-
-			var pixelWidth = width/cols; 
-			var pixelHeight = height/rows; 
-				
-			canvas.attr("width",width).attr("height",height);
-			
-			var context = canvas.node().getContext("2d");
-			
-			
-			data.forEach(function(d,i) {
-				// console.log(d,i);
-				// context.beginPath();
-				d.forEach(function(q,l) {
-					context.beginPath();
-					context.rect(l*pixelWidth, i*pixelHeight, pixelWidth, pixelHeight);
-					context.lineWidth="-1";
-					context.fillStyle="hsl("+maincolor+", 100%,"+q+"%)";
-					context.fill();
-					// context.stroke();
-				});
-				// context.closePath();
-			});
-
-		}
-  	};
+  }
+ };
 }]);
+
+
+
+
