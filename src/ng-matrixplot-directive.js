8e1e2da0ed9ef146a636c77f37a9565edeaddb57
Alejandro Ubuntu Uni <alejandroamsg@hotmail.com>
2016-02-19 18:53:06 +0100

    Fixed bug for the pure javascript module, settings and canvas are now attributes of the MatrixPlotClass

--- src/ng-matrixplot-directive.js
+++ src/ng-matrixplot-directive.js
@@ -1,21 +1,21 @@
-angular.module("ng-matrixplot", []).directive("matrixPlot", 
-  ["$parse", function ($parse) {
- return {
-  restrict: "EA",
-  template: "<canvas> </canvas>",
-  link: function(scope, elem, attrs){
-   var canvas = elem.find("canvas")[0];
-   var settings = $parse(attrs.settings)(scope);
-   var plot = matrixplot(canvas, settings);
-   scope.$watch(attrs.settings, 
-    function(newv, oldv) {
-      if (newv) {
-       plot.refresh(newv); 
-      }
-     },true);
+angular.module("ng-matrixplot", [])
+.directive("matrixPlot", ["$parse", function ($parse) {
+  return {
+    restrict: "EA",
+    template: "<canvas> </canvas>",
+    link: function(scope, elem, attrs){
+      var canvas   = elem.find("canvas")[0];
+      var settings = $parse(attrs.settings)(scope);
+      var plot     = matrixplot(canvas, settings);
+      scope.$watch(attrs.settings,
+          function(newv, oldv) {
+            if (newv) {
+              plot.refresh(newv);
+            }
+          },true);
     }
-   }; 
-  }]);
+  };
+}]);
 
 
 
